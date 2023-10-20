906696640d5aaad56a27cfb1b8c9f1bb096980b5
syke99 <quinnmillican@gmail.com>
2023-10-20 08:46:01 -0600

    func map

--- client/oasis.go
+++ client/oasis.go
@@ -4,20 +4,32 @@ package client
 
 import "syscall/js"
 
+type FuncMap map[string]func(args ...js.Value)
+
 type Oasis struct {
-	funcs map[string]func(args ...js.Value)
+	funcs FuncMap
 }
 
 func NewOasis() *Oasis {
 	return &Oasis{}
 }
 
-func (o *Oasis) AddFunc(name string, fn func(args ...js.Value)) {
+func (o *Oasis) AddToFuncMap(name string, fn func(args ...js.Value)) {
 	if o.funcs == nil {
-		o.funcs = make(map[string]func(args ...js.Value))
+		o.funcs = make(FuncMap)
 	}
 
 	o.funcs[name] = fn
+}
+
+func (o *Oasis) AddFuncMap(fmap FuncMap) {
+	if o.funcs == nil {
+		o.funcs = fmap
+		return
+	}
+	for k, v := range fmap {
+		o.funcs[k] = v
+	}
 }
 
 func (o *Oasis) Run() {
